2401115c16040c00383cb6f6fe9dd225911c0506
lxm <3195485648@qq.com>
2022-09-12 20:05:26 +0800

    ffd

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+#app,
+body {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background-color: black !important;
+  color: rgba(0, 0, 0, 0.65);
+  font-size: 14px;
+}
+</style>
